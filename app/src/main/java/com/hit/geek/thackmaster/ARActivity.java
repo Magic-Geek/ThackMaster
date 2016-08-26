55f6c03dff13e50ea52ebfaccda99e790a06b8ae
dusz7 <dusz2599@163.com>
2016-08-26 11:20:02 +0800

    ar配置成功！赶紧先备份下

--- app/src/main/java/com/hit/geek/thackmaster/ARActivity.java
+++ app/src/main/java/com/hit/geek/thackmaster/ARActivity.java
@@ -1,22 +1,95 @@
 package com.hit.geek.thackmaster;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import cn.easyar.engine.EasyAR;
 
 /**
  * Created by dusz2 on 2016/8/26 0026.
  */
 public class ARActivity extends Activity {
 
+    static String key = "0MKt9udgV8P4ZJ9UfjWVv4Knr7UW1UaQoyUnS57I3fqTA4ItzeRQV0hNN5JPvLq2jZOU628X0ZsRvrDSKxar6FtYTBj1y29e7YM715ae3e24343170c07cfa9a7e1ee85f57qhDDAJHj9MkJLvNeRXZLNlQnR2YOVC91ZEx4kadveXg4sbTOitZYmreJOYKveDlXuDxa";
+
+    static {
+        System.loadLibrary("EasyAR");
+        System.loadLibrary("HelloARVideoNative");
+    }
+
+    public static native void nativeInitGL();
+    public static native void nativeResizeGL(int w, int h);
+    public static native void nativeRender();
+    private native boolean nativeInit();
+    private native void nativeDestory();
+    private native void nativeRotationChange(boolean portrait);
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ar);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        EasyAR.initialize(this, key);
+        nativeInit();
+
+        GLView glView = new GLView(this);
+        glView.setRenderer(new Renderer());
+        glView.setZOrderMediaOverlay(true);
+
+        ((ViewGroup) findViewById(R.id.preview)).addView(glView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        nativeRotationChange(getWindowManager().getDefaultDisplay().getRotation() == android.view.Surface.ROTATION_0);
+
 //        Intent intent = new Intent();
 //        setResult(RESULT_OK, intent);
 //        finish();
 
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration config) {
+        super.onConfigurationChanged(config);
+        nativeRotationChange(getWindowManager().getDefaultDisplay().getRotation() == android.view.Surface.ROTATION_0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        nativeDestory();
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        EasyAR.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        EasyAR.onPause();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 
