89e36074fbf2a066f5beec3ed24debd0c661acd0
dusz7 <dusz2599@163.com>
2016-08-26 10:37:40 +0800

    项目初步的搭建

---
+++ app/src/main/java/com/hit/geek/thackmaster/ARActivity.java
@@ -0,0 +1,24 @@
+package com.hit.geek.thackmaster;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by dusz2 on 2016/8/26 0026.
+ */
+public class ARActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ar);
+
+//        Intent intent = new Intent();
+//        setResult(RESULT_OK, intent);
+//        finish();
+
+    }
+
+
+
+}
---
+++ app/src/main/java/com/hit/geek/thackmaster/MainActivity.java
@@ -0,0 +1,32 @@
+package com.hit.geek.thackmaster;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void ar_click(View view){
+        Intent intent = new Intent(MainActivity.this,ARActivity.class);
+        startActivityForResult(intent, 0);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+
+
+
+        }
+    }
+}
